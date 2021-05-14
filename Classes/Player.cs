8bd19aef803898258d9404769f4f9eb70bb70c4a
Rippan <Jepperipnas@gmail.com>
2021-05-15 00:39:32 +0200

    Added folder structure & updated .gitignore

---
+++ Classes/Player.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+/// <summary>
+/// Summary description for Class1
+/// </summary>
+public class Player
+{
+	public Player()
+	{
+		//
+		// TODO: Add constructor logic here
+		//
+	}
+}
