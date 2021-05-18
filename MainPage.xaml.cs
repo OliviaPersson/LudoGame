8a3e9fed6b378d19baf2233c17b6e3e63751f570
Rippan <Jepperipnas@gmail.com>
2021-05-18 13:08:43 +0200

    Added Scaler Class

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -1,10 +1,17 @@
-﻿using System;
+﻿using Microsoft.Graphics.Canvas;
+using Microsoft.Graphics.Canvas.UI;
+using Microsoft.Graphics.Canvas.UI.Xaml;
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
+using System.Threading.Tasks;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.UI;
+using Windows.UI.Core;
+using Windows.UI.ViewManagement;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
@@ -22,9 +29,42 @@ namespace LudoGame
     /// </summary>
     public sealed partial class MainPage : Page
     {
+        CanvasBitmap backgroundImage;
+        public static Rect bounds = ApplicationView.GetForCurrentView().VisibleBounds;
+        public static float GameWidth = 720;
+        public static float GameHeight = 1280;
+        public static float scaleWidth, scaleHeight;
+
         public MainPage()
         {
             this.InitializeComponent();
+            Window.Current.SizeChanged += Current_SizeChanged;
+            Scaler.SetScale();
+        }
+
+        private void Current_SizeChanged(object sender, WindowSizeChangedEventArgs e)
+        {
+            bounds = ApplicationView.GetForCurrentView().VisibleBounds;
+            Scaler.SetScale();
+        }
+
+        private void GameCanvas_CreateResources(CanvasControl sender, CanvasCreateResourcesEventArgs args)
+        {
+            args.TrackAsyncAction(CreateResources(sender).AsAsyncAction());
+        }
+        async Task CreateResources(CanvasControl sender)
+        {
+            backgroundImage = await CanvasBitmap.LoadAsync(sender, new Uri("ms-appx:///Assets/Images/bg.png"));
+        }
+
+        private void GameCanvas_Draw(CanvasControl sender, CanvasDrawEventArgs args)
+        {
+            args.DrawingSession.DrawImage(Scaler.Img(backgroundImage));
+            GameCanvas.Invalidate();
+
+        }
+        private void GameCanvas_Tapped(object sender, TappedRoutedEventArgs e)
+        {
 
         }
     }
