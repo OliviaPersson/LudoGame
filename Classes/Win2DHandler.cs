2309b7942f36f08fa514fcdf64a608c030a37eb2
Olivia Persson <Olivia__P@hotmail.com>
2021-05-19 20:52:47 +0200

    Added tile images for game map

--- Classes/Win2DHandler.cs
+++ Classes/Win2DHandler.cs
@@ -6,12 +6,13 @@ using System.Text;
 using System.Threading.Tasks;
 using Microsoft.Graphics.Canvas;
 using Microsoft.Graphics.Canvas.UI.Xaml;
+using Windows.UI;
 
 namespace LudoGame
 {
     public class Win2DHandler
     {
-        CanvasBitmap backgroundImage, blackholeImage, menuBackGroundImage;
+        CanvasBitmap backgroundImage, blackholeImage, menuBackGroundImage, greenBaseImg, blueBaseImg, redBaseImg, yellowBaseImg, whiteTileImg, yellowTileImg, blueTileImg, greenTileImg, redTileImg;
 
         public void Update()
         {
@@ -26,6 +27,9 @@ namespace LudoGame
         }
         public void Draw(CanvasAnimatedDrawEventArgs args)
         {
+            float width = (float)MainPage.bounds.Width;
+            float height = (float)MainPage.bounds.Height;
+
             switch (GameEngine.CurrentGameState)
             {
                 case 0:
@@ -39,6 +43,15 @@ namespace LudoGame
                 case 3: break;
             }
         }
+
+        /// <summary>
+        /// Not yet implemented Calculate positions and draw tiles
+        /// </summary>
+        public void DrawMap(CanvasAnimatedDrawEventArgs args)
+        {
+
+        }
+
         // Load Asset
         public async Task CreateResources(CanvasAnimatedControl sender)
         {
@@ -48,6 +61,17 @@ namespace LudoGame
             // Playing
             backgroundImage = await CanvasBitmap.LoadAsync(sender, new Uri("ms-appx:///Assets/Images/bg.png"));
             blackholeImage = await CanvasBitmap.LoadAsync(sender, new Uri("ms-appx:///Assets/Images/blackhole.png"));
+
+            //Load map
+            greenBaseImg = await CanvasBitmap.LoadAsync(sender, new Uri("ms-appx:///Assets/Images/Tiles/GreenBase.png"));
+            blueBaseImg = await CanvasBitmap.LoadAsync(sender, new Uri("ms-appx:///Assets/Images/Tiles/BlueBase.png"));
+            redBaseImg = await CanvasBitmap.LoadAsync(sender, new Uri("ms-appx:///Assets/Images/Tiles/RedBase.png"));
+            yellowBaseImg = await CanvasBitmap.LoadAsync(sender, new Uri("ms-appx:///Assets/Images/Tiles/YellowBase.png"));
+            whiteTileImg = await CanvasBitmap.LoadAsync(sender, new Uri("ms-appx:///Assets/Images/Tiles/WhiteTile.png"));
+            yellowTileImg = await CanvasBitmap.LoadAsync(sender, new Uri("ms-appx:///Assets/Images/Tiles/YellowTile.png"));
+            blueTileImg = await CanvasBitmap.LoadAsync(sender, new Uri("ms-appx:///Assets/Images/Tiles/BlueTile.png"));
+            greenTileImg = await CanvasBitmap.LoadAsync(sender, new Uri("ms-appx:///Assets/Images/Tiles/GreenTile.png"));
+            redTileImg = await CanvasBitmap.LoadAsync(sender, new Uri("ms-appx:///Assets/Images/Tiles/RedTile.png"));
         }
     }
 }
