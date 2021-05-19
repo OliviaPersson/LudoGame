705d6da61ef3695cd83e3c76c67f156495a18681
KonstigSwe <swekonstig@gmail.com>
2021-05-19 11:05:48 +0200

    Added coments

--- Classes/FileHandeler.cs
+++ Classes/FileHandeler.cs
@@ -9,20 +9,32 @@ namespace LudoGame.Classes
 {
     static class FileHandeler
     {
+        /// <summary>
+        /// Not yet implemented load images class
+        /// </summary>
         public static CanvasBitmap LoadSprites (string filelocation)
         {//Return NYI
 
             return null;
         }
+        /// <summary>
+        /// Not yet implemented load sound class
+        /// </summary>
         //public static Sound LoadSounds(string filelocation)
         //{//Return NYI
 
         //    return null; 
         //}
+        /// <summary>
+        /// Not yet implemented save game class
+        /// </summary>
         public static void Save()
         {
 
         }
+        /// <summary>
+        /// Not yet implemented load game class
+        /// </summary>
         public static void Load()
         {
 
