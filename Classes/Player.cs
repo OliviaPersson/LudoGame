dd8ba4671b834a6a4fb0e76edbe9c3586507ca92
KonstigSwe <swekonstig@gmail.com>
2021-05-26 11:48:40 +0200

    Added some Planning? comments on AIPlayer and Player Also added inherance from player to AIPlayer

--- Classes/Player.cs
+++ Classes/Player.cs
@@ -8,7 +8,9 @@ using System.Threading.Tasks;
 namespace LudoGame.Classes
 {
     class Player
+
     {
+        // Color for the team /color you are is to be added. 
         public GamePiece[] GamePieces = new GamePiece[4];
         public CanvasBitmap GamePiceSprite;
         public void RollDice()
@@ -16,4 +18,5 @@ namespace LudoGame.Classes
             int result = Dice.randomNum();
         }
     }
+  
 }
